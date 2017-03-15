b1b606a39227bbef6c39aad7950aeafba08678cd
Gritsenko S.D. <gritsenkosd@gmail.com>
2017-03-15 11:04:56 +0200

    Add project files.

---
+++ DragablzCustomTab/MainInterTabClient.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using Dragablz;
+
+namespace DragablzCustomTab
+{
+    class MainInterTabClient : IInterTabClient
+    {
+        public INewTabHost<System.Windows.Window> GetNewHost(IInterTabClient interTabClient, object partition, TabablzControl source)
+        {
+            var view = new TabHostWindow();
+            return new NewTabHost<TabHostWindow>(view, view.TabsContainer);
+        }
+
+        public TabEmptiedResponse TabEmptiedHandler(TabablzControl tabControl, Window window)
+        {
+            return TabEmptiedResponse.CloseWindowOrLayoutBranch;
+        }
+    }
+}
---
+++ DragablzCustomTab/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Dragablz;
+
+namespace DragablzCustomTab
+{
+    class MainWindowViewModel
+    {
+        public IInterTabClient InterTabClient { get; set; }
+        public MainWindowViewModel()
+        {
+            InterTabClient = new MainInterTabClient();
+        }
+    }
+}
